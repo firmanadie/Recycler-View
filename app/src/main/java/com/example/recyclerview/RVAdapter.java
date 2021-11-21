c64b9b0948ecf4dc1eaa2bfeb9dfb141cc413a52
firmanadiafuah <firdifuah17@gmail.com>
2021-11-22 00:00:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/RVAdapter.java
@@ -0,0 +1,68 @@
+package com.example.recyclerview;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recyclerview.databinding.ItemRecyclerViewBinding;
+
+import java.util.ArrayList;
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.GridViewHolder> {
+    private ArrayList<Teman> temanArrayList;
+    private Context context;
+    private ItemRecyclerViewBinding itemRecyclerViewBinding;
+
+    public RVAdapter(Context context, ArrayList<Teman> temanArrayList) {
+        this.context = context;
+        this.temanArrayList = temanArrayList;
+    }
+
+    @NonNull
+    @Override
+    public GridViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        itemRecyclerViewBinding = ItemRecyclerViewBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new GridViewHolder(itemRecyclerViewBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GridViewHolder holder, int position) {
+        final String
+                nama = temanArrayList.get(position).getNama(),
+                alamat = temanArrayList.get(position).getAlamat(),
+                telepon = temanArrayList.get(position).getTelepon();
+
+        itemRecyclerViewBinding.tvNama.setText(nama);
+        itemRecyclerViewBinding.tvAlamat.setText(alamat);
+        itemRecyclerViewBinding.tvTelepon.setText(telepon);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, nama, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return temanArrayList.size();
+    }
+
+    public class GridViewHolder extends RecyclerView.ViewHolder {
+        ItemRecyclerViewBinding itemRecyclerViewBinding;
+
+        public GridViewHolder(@NonNull ItemRecyclerViewBinding binding) {
+            super(binding.getRoot());
+            itemRecyclerViewBinding = binding;
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/recyclerview/Teman.java
@@ -0,0 +1,24 @@
+package com.example.recyclerview;
+
+public class Teman {
+    String nama, telepon, alamat;
+
+    public Teman(String nama, String telepon, String alamat) {
+        this.nama = nama;
+        this.telepon = telepon;
+        this.alamat = alamat;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+}
+
